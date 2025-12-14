1677fb2482d99e61656393785ebd7330c0e263a3
jhonh <jmherrera11@espe.edu.ec>
2025-12-14 13:22:20 -0500

    Iteraccion1: add devuelve 0 para cadena vacia HerreraJhon

---
+++ src/main/java/com/mycompany/stringcalculator/StringCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stringcalculator;
+
+/**
+ *
+ * @author FAMILIA HERRERA
+ */
+public class StringCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
