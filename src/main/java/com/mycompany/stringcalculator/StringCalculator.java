b08688758f579a92a77b3723c23a0a3f054ffc16
jhonh <jmherrera11@espe.edu.ec>
2025-12-14 21:18:03 -0500

    Iteración3: add devuelve el la suma de los numeros 1,2 separados por coma

--- src/main/java/com/mycompany/stringcalculator/StringCalculator.java
+++ src/main/java/com/mycompany/stringcalculator/StringCalculator.java
@@ -3,6 +3,9 @@ package com.mycompany.stringcalculator;
 
 public class StringCalculator {
        public int add(String numbers) {
+           if(numbers.isEmpty()){
+               return 0;
+           }
         return 0;
     }
 }
--- src/test/java/com/mycompany/stringcalculator/StringAppTest.java
+++ src/test/java/com/mycompany/stringcalculator/StringAppTest.java
@@ -1,13 +1,22 @@
-
 package com.mycompany.stringcalculator;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 public class StringAppTest {
    @Test
     void testEmptyStringReturnsZero() {
         StringCalculator calculator = new StringCalculator();
         assertEquals(0, calculator.add(""));
+    }
+    
+    @Test
+    void testSingleNumberReturnsValue() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+    }
+    
+    @Test
+    void testTwoNumbersReturnSum() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(3, calculator.add("1,2"));
     }
 }
