f9da26285003060b9e2e00c12ab2084952ffd6b7
Herrera Jhon <jmherrera11@espe.edu.ec>
2025-12-14 23:11:47 -0500

    Fix: Revertir falla intencional del pipeline Herrera Jhon

--- src/test/java/com/mycompany/stringcalculator/StringAppTest.java
+++ src/test/java/com/mycompany/stringcalculator/StringAppTest.java
@@ -5,7 +5,7 @@ public class StringAppTest {
    @Test
     void testEmptyStringReturnsZero() {
         StringCalculator calculator = new StringCalculator();
-        assertEquals(5, calculator.add(""));
+        assertEquals(0, calculator.add(""));
     }
     
     @Test
